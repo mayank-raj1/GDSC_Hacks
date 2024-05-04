4b1c1aad248ce9e171605538631ea830b88d2b08
ash <ash@localhost.localdomain>
2024-05-04 12:11:57 -0400

    tailwind finally working, some styling added

--- Frontend/src/components/dashboard/NavBar.jsx
+++ Frontend/src/components/dashboard/NavBar.jsx
@@ -1,22 +1,47 @@
 import { Outlet } from "react-router";
+import { NavLink } from "react-router-dom";
+
 
 function NavBar() {
 
+  const navLinks = [
+    {
+      name: "Contacts",
+      route: "/contacts"
+    },
+    {
+      name: "Chat",
+      route: "/chat"
+    },
+    {
+      name: "Other",
+      route: "/other"
+    },
+  ]
+
   return (
-    <div className="h-full w-16 border border-slate-300">
-      <nav>
-        <h1 className="">
-          ProVision
-        </h1>
-        <ul>
-
-        </ul>
-      </nav>
-
+    <div className="flex h-screen w-full">
+      <div className="border-r border-slate-300 h-full">
+        <nav>
+          <h1 className="text-4xl m-6">ProVision</h1>
+          <ul>
+            {
+              navLinks.map((link, i) => {
+                return (
+                  <li key={i} className="ml-4 text-xl">
+                    <NavLink>
+                      <h3>{link.name}</h3>
+                    </NavLink>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </nav>
+      </div>
       <Outlet />
-
     </div>
-  );
+  )
 }
 
 export default NavBar;
