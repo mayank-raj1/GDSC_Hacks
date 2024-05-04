b6ff1e83d8cc3c884338ec968e72bc7def1c1772
ash <ash@localhost.localdomain>
2024-05-04 11:03:43 -0400

    adding new files, basic router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,22 @@
 import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard.jsx';
+import NavBar from './components/dashboard/NavBar.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar />,
+  },
+
+])
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ Frontend/src/components/dashboard/NavBar.jsx
@@ -0,0 +1,19 @@
+
+function NavBar() {
+
+  return (
+    <div>
+      <nav>
+        <h1 className="">
+          ProVision
+        </h1>
+        <ul>
+
+        </ul>
+      </nav>
+
+    </div>
+  );
+}
+
+export default NavBar;
