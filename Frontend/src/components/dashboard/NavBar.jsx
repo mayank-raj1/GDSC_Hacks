a0ce3e912d50cc1a47bcd0575c172a1a71f0eab2
ash <ash@localhost.localdomain>
2024-05-04 13:27:28 -0400

    styling navbar and contacts

--- Frontend/src/components/dashboard/NavBar.jsx
+++ Frontend/src/components/dashboard/NavBar.jsx
@@ -29,18 +29,18 @@ function NavBar() {
 
   return (
     <div className="flex min-h-screen max-h-full w-full">
-      <div className="border-r border-slate-300 bg-white h-full">
+      <div className="border-r border-slate-300 bg-white h-full w-20 hover:w-56 transition-all">
         <nav>
           <h1 className="text-4xl m-6">ProVision</h1>
           <ul>
             {
               navLinks.map((link, i) => {
                 return (
-                  <li key={i} className="ml-4 mb-6 text-xl">
+                  <li key={i} className="ml-6 mb-10 text-xl">
                     <NavLink to={link.route}>
-                      <div className="flex items-center">
-                        <i className="material-icons mr-4">{link.icon}</i>
-                        <h3 className="w-fit">{link.name}</h3>
+                      <div className="flex items-center w-fit">
+                        <i className="material-icons w-8 mr-7 text-3xl">{link.icon}</i>
+                        <h3 className="w-fit ">{link.name}</h3>
                       </div>
                     </NavLink>
                   </li>
